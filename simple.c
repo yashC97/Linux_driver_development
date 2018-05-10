33af14807300e964545534f87988c93b6d9ff640
yash.bhatt <yash.bhatt1@einfochips.com>
2018-05-10 14:40:27 +0530

    Nothing

--- simple.c
+++ simple.c
@@ -13,7 +13,7 @@ MODULE_VERSION("0.1");              ///< The version of the module
 
 static char *name = "ModuleSimple";        ///< An example LKM argument -- default value is "world"
 module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
-MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description""))"""""")"")"")"")
+MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description
 
 static int simple_init(void)
 {
